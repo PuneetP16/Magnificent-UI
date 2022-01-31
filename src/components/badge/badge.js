e6d72626957a335b1246a34b401191971301f3f6
PuneetP16 <patelpunit95@gmail.com>
2022-01-31 17:24:16 +0530

    refactor: formatted code and doc

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -1,11 +1,8 @@
-
 const toggleBadgeVisibility = () => {
-    let badgeElement = document.getElementById("badgetoggle1")
-    if (badgeElement.style.display === "none") {
-        badgeElement.style.display = "flex";
-      } else {
-        badgeElement.style.display = "none";
-      }
-}
-
-
+	let badgeElement = document.getElementById("badgetoggle1");
+	if (badgeElement.style.display === "none") {
+		badgeElement.style.display = "flex";
+	} else {
+		badgeElement.style.display = "none";
+	}
+};
