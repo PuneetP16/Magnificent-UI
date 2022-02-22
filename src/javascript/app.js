fe3e58674e3e7ad9fe9b09dda60c9f7457e31eff
PuneetP16 <patelpunit95@gmail.com>
2022-02-22 22:40:47 +0530

    refactor: structure code

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,5 +1,7 @@
+// Hamburger icon element
 const hamburgerBtn = document.querySelector(".hamburger_icon");
 
+// function for Mobile Navigation menu
 hamburgerBtn.addEventListener("click", () => {
 	document.querySelector("body").classList.toggle("show");
 	document.body.scrollTop = 0;
